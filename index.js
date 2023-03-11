d13e04a0dae954c7d4a4ea2f3a7fda007d083b79
kevin Wairi <kevinwairi@gmail.com>
2023-03-11 12:52:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(num1,num2){
+    return `${num1} + ${num2}`;
+}
+function subtract(num1,num2){
+    return `${num1} - ${num2}`;
+}
+function multiply(num1,num2){
+    return `${num1} * ${num2}`;
+}
+function divide(num1,num2){
+    return `${num1} / ${num2}`;
+}
+
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+
+function  increment(n){
+    return ++n;
+}
+function  decrement(n){
+    return --n;
+}
+
+function makeInt(n){
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
